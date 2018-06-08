ffeb394cbd245b36d7d6f398eaacb6f78e15b548
Ion Mihnea <38386852+MihneaIon@users.noreply.github.com>
2018-06-08 19:58:42 +0300

    Add files via upload

---
+++ src/main/java/entity/Nivel.java
@@ -0,0 +1,16 @@
+package entity;
+
+public enum Nivel {
+	ERROR, WARN, INFO;
+
+//	public int value;
+//	
+//	Nivel(int valueArg) {
+//		value=valueArg;
+//	}
+//	private Nivel() {
+//		
+//	}
+	
+	
+}
